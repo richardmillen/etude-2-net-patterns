27371ffd7bb1ed871f00936f09e7301fbe4eb522
richardmillen <develop@richardmillen.com>
2018-01-26 15:42:56 +0000

    moved batch test run into separate bash script.

--- src-go/build.sh
+++ src-go/build.sh
@@ -4,53 +4,6 @@ if [ ! -d "../bin/" ]; then
     mkdir ../bin/
 fi
 
-if [[ $1 == "" || $1 == "test" ]]; then
-    printf "\nrunning package tests...\n"
-    printf "\n"
-    printf "\t- - - - - - - - - -\n"
-    printf "\t- frames          -\n"
-    printf "\t- patterns/core   -\n"
-    printf "\t- patterns/disco  -\n"
-    printf "\t- patterns/pubsub -\n"
-    printf "\t- utils            -\n"
-    printf "\t- uuid            -\n"
-    printf "\t- - - - - - - - - -\n"
-    printf "\n"
-
-    cd frames
-    go test -v -cover
-    cd ..
-    printf "\n"
-
-    cd patterns/core
-    go test -v -cover
-    cd ../..
-    printf "\n"
-
-    cd patterns/disco
-    go test -v -cover
-    cd ../..
-    printf "\n"
-
-    cd patterns/pubsub
-    go test -v -cover
-    cd ../..
-    printf "\n"
-
-    cd utils
-    go test -v -cover
-    cd ..
-    printf "\n"
-
-    cd uuid
-    go test -v -cover
-    cd ..
-    printf "\n"
-
-    printf "finished running tests.\n"
-    printf "\n"
-fi
-
 cd apps
 
 
