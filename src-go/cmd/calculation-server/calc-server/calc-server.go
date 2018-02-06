590560fa6c2f8923b6bda521b9df20c95bb069d4
richardmillen <develop@richardmillen.com>
2018-02-06 21:59:50 +0000

    added sketch of 'hello world' client/server example.
    started sketching calculation server.

--- src-go/cmd/calculation-server/calc-server/calc-server.go
+++ src-go/cmd/calculation-server/calc-server/calc-server.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/richardmillen/etude-2-net-patterns/src-go/check"
 )
 
 var port = flag.Int("port", 5432, "port number to listen at")
 
 func main() {
+	flag.Parse()
+	
 	log.Println("starting calc-server...")
 
-	// ....
+	l, err := netx.ListenTCP("tcp", fmt.Sprintf(":%d", *port))
+	check.Error(err)
+
+	svc := service.New(l)
+
+	recvState := state.New("receiving")
+	recvState.Accept(&)
+
+	invalidState := state.New("invalid message")
+	invalidState.AddSubstate(recvState)
+
+	svc.Initial(recvState)
+
+	svc.OnRecv(func(f []netx.Frame) error {
+
+	})
 
 	log.Println("calc-server started.")
 
